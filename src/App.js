c2bac8478a999e93aac95b878edd74290c5c7e15
Anand K H <khanand1998@gmail.com>
2020-11-17 19:56:12 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Route,BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
-
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Report from './pages/Report';
+import Team from './pages/Team';
+import Message from './pages/Message';
+import Support from './pages/Support';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Router>
+      <Sidebar/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/product" component={Product}/>
+          <Route path="/report" component={Report}/>
+          <Route path="/team" component={Team}/>
+          <Route path="/message" component={Message}/>
+          <Route path="/support" component={Support}/>
+        </Switch>
+      </Router>
+      
+      </>
     </div>
   );
 }
